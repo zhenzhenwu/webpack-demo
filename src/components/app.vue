40d73e33ece482554f5ae299de4da28ecd585384
wuqianang <wuqianang@163.com>
2018-06-24 18:17:45 +0800

    0624

---
+++ src/components/app.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>{{ message }}</h1>
+</template>
+
+<script>
+    // 这是一个组件但它并没有绑定到具体的元素
+    export default {
+        name: 'App',
+        data () {
+            return {
+                message: 'Hi! zhenzhen'
+            }
+        }
+    }
+</script>
+
+<style lang="css">
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+    }
+</style>
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     entry: {
@@ -22,7 +23,7 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     { loader: 'style-loader'},
-                    { loader: 'css-loader', options: { module: true } }
+                    { loader: 'css-loader' }
                 ]
             },
             {
@@ -30,13 +31,27 @@ module.exports = {
                 use: [
                     'file-loader'
                 ]
+            },
+            {
+                test: /\.vue$/,
+                use: 'vue-loader'
             }
         ]
     },
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',   //  https://cn.vuejs.org/v2/guide/installation.html#%E5%AF%B9%E4%B8%8D%E5%90%8C%E6%9E%84%E5%BB%BA%E7%89%88%E6%9C%AC%E7%9A%84%E8%A7%A3%E9%87%8A 
+            '@': path.join(__dirname, '..', 'src')
+        }
+    },
     plugins: [
         new CleanWebpackPlugin(['dist']),   // 清理/dist文件夹
-        new HtmlWebpackPlugin({         // 生成HTML文件，并动态添加bundle
-            title: 'Output Management'
-        })
+        new HtmlWebpackPlugin({             // 生成HTML文件，并动态添加bundle
+            filename: 'index.html',         
+            template: 'index.html',         // 使用模板文件
+            inject: true
+        }),
+        new VueLoaderPlugin()
     ]
 }
