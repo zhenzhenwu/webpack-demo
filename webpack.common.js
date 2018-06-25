19b3a987c4dc2e6c18df2f2020f7cbf378b2b46f
wuqianang <wuqianang@163.com>
2018-06-25 15:08:38 +0800

    '0625'

--- webpack.common.js
+++ webpack.common.js
@@ -65,6 +65,6 @@ module.exports = {
             template: 'index.html',         // 使用模板文件
             inject: true
         }),
-        new VueLoaderPlugin()
+        new VueLoaderPlugin()   // **这个插件是必须的！**它的职责是将你定义过的其它规则复制并应用到 .vue 文件里相应语言的块。例如，如果你有一条匹配 /\.js$/ 的规则，那么它会应用到 .vue 文件里的 <script> 块。
     ]
 }
